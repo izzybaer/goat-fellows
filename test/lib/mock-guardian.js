d6218133a09106bc0761b66cd09fcfdabaa39c5f
dustin <dbyers84@gmail.com>
2017-07-11 10:26:12 -0700

    the GOAT is now being Mocked

--- test/lib/mock-guardian.js
+++ test/lib/mock-guardian.js
@@ -8,40 +8,40 @@ const mockGuardian = module.exports = {};
 mockGuardian.createOne = () => {
   let result = {};
   return mockUser.createOne()
-  .then((user) => {
-    result.user = user;
-    return new Guardian({
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email(),
-      city: faker.address.city(),
-      state: faker.random.us_state_abbr(),
-      service: faker.random.bs_buzz(),
-      phoneNumber: faker.PhoneNumber.phoneNumber(),
-      userID: result.user._id.toString(),
+    .then((user) => {
+      result.user = user;
+      return new Guardian({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        state: faker.random.us_state_abbr(),
+        service: faker.random.bs_buzz(),
+        phoneNumber: faker.PhoneNumber.phoneNumber(),
+        userID: result.user._id.toString(),
+      });
     });
-  });
 };
 
 mockGuardian.createMany = (n) => {
   let result = {};
   return mockUser.createOne()
-  .then(user => {
-    result.user = user;
-    let guardiansToSave = new Array(n).fill(0).map(() => new Guardian({
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email(),
-      city: faker.address.city(),
-      state: faker.random.us_state_abbr(),
-      service: faker.random.bs_buzz(),
-      phoneNumber: faker.PhoneNumber.phoneNumber(),
-      userID: result.user._id.toString(),
-    }).save());
-    return Promise.all(guardiansToSave);
-  })
-  .then(guardians => {
-    result.guardians = guardians;
-    return result;
-  });
+    .then(user => {
+      result.user = user;
+      let guardiansToSave = new Array(n).fill(0).map(() => new Guardian({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        state: faker.random.us_state_abbr(),
+        service: faker.random.bs_buzz(),
+        phoneNumber: faker.PhoneNumber.phoneNumber(),
+        userID: result.user._id.toString(),
+      }).save());
+      return Promise.all(guardiansToSave);
+    })
+    .then(guardians => {
+      result.guardians = guardians;
+      return result;
+    });
 };
