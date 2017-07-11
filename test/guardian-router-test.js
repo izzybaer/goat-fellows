aee65d34b14f7efc4134d7864aeb863ba1f6aa0f
dustin <dbyers84@gmail.com>
2017-07-11 09:58:04 -0700

    guardian got straight MOCKED< sonnnnn

---
+++ test/guardian-router-test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('dotenv').config({path: `${__dirname}/../.test.env`});
+const superagent = require('superagent');
+const expect = require('expect');
+const faker = require('faker');
+const clearDB = require('./lib/clearDB.js');
+const guardianRouter = require('../route/guardian-router.js');
+const mockUser = require('./lib/moc11k-user.js');
+const server = require('../lib/server.js');
+const API_URL = process.env.API_URL;
+
+describe('testing guardian-routs', () => {
+
+  describe('testing api/guardian ', () => {
+
+  });
+});
