d88918ff9c667424c611ed3240cda5586fdc3bd8
mikeporter <porter.michael.james@gmail.com>
2017-07-11 17:17:39 -0700

    didn't finish any goat tests

--- minutes.md
+++ minutes.md
@@ -2,10 +2,16 @@
 
 # we are 60% til mvp
 
-# have all models built, all routes built, all middleware built, by end of day today will be finished with user tests, and the user model is one of three models. 
+# have all models built, all routes built, all middleware built, by end of day today will be finished with user tests, and the user model is one of three models.
 
 # tomorrow by noon finish all tests (MVP)
 
 # start talking about stretch goals tomorrow afternoon
 
 # Duncan says 'SWEET, Y'ALL KEEP IT UP'
+
+# Stand up day 2
+
+90% done,
+100% and 90% code coverage tomorrow
+thursday do a stretch goal and write readme in the afternoon
--- test/goat-router-test.js
+++ test/goat-router-test.js
@@ -1,19 +1,17 @@
 // 'use strict';
 //
 // require('dotenv').config({path: `${__dirname}/../.test.env`});
+// require('./lib/mock-aws.js');
 // const superagent = require('superagent');
 // const expect = require('expect');
 // const faker = require('faker');
 //
 // const clearDB = require('./lib/clearDB.js');
-// const Guardian = require('../model/guardian.js');
-// const mockUser = require('./lib/mock-user.js');
 // const mockGoat = require('./lib/mock-goat.js');
-// // const mockGuardian = require('./lib/mock-guardian.js');
+// const mockGuardian = require('./lib/mock-guardian.js');
 //
 //
 // const server = require('../lib/server.js');
-//
 // const API_URL = process.env.API_URL;
 //
 // describe('testing routes for goat model', () => {
@@ -21,45 +19,28 @@
 //   after(server.stop);
 //   afterEach(clearDB);
 //
-//   describe('testing POST /api/goat', () => {
+//   describe.only('testing POST /api/goats', () => {
 //     it('should return a new goat and respond with 200', () => {
-//       let tempUser, tempGoat, tempGuardian;
-//       return mockUser.createOne()
-//         .then((userData) => {
-//           tempUser = userData;
-//           return new Guardian({
-//             firstName: faker.name.firstName(),
-//             lastName: faker.name.lastName(),
-//             email: faker.internet.email(),
-//             city: faker.address.city(),
-//             state: faker.address.stateAbbr(),
-//             service: faker.company.bsBuzz(),
-//             phoneNumber: faker.phone.phoneNumber(),
-//             userID: userData.user._id.toString(),
-//           });
-//         })
-//         .then(guardian => {
-//           tempGuardian = guardian;
-//           return mockGoat.createOne();
-//         })
-//         .then(goat => {
-//           tempGoat = goat;
-//           console.log('tempUser:\n', tempUser);
-//           console.log('tempGoat:\n', tempGoat);
-//           console.log('tempGuardian:\n', tempGuardian);
-//           return superagent.post(`${API_URL}/api/goat`)
-//             .set('Authorization', `Bearer ${tempUser.token}`)
-//             .send(tempGoat);
-//         })
-//         .then(res => {
-//           expect(res.status).toEqual(200);
-//           expect(res.body.city).toEqual(tempGoat.city);
-//           expect(res.body.state).toEqual(tempGoat.state);
-//           expect(res.body.address).toEqual(tempGoat.address);
-//           expect(res.body.photoURI).toEqual(tempGoat.photoURI);
-//           expect(res.body.story).toEqual(tempGoat.story);
-//           expect(res.body.userID).toEqual(tempGoat.userID);
-//           expect(res.body.guardianID).toEqual(tempGoat.guardianID);
+//       let tempData;
+//       let tempAddress = faker.address.streetAddress();
+//       let tempCity = faker.address.city();
+//       let tempState = faker.address.stateAbbr();
+//       let tempStory = faker.lorem.sentence();
+//       return mockGuardian.createOne()
+//         .then((userGoatData) => {
+//           tempData = userGoatData;
+//           return superagent.post(`${API_URL}/api/goats`)
+//             .set('Authorization', `Bearer ${tempData.user.user.token}`)
+//             .field('address', tempAddress)
+//             .field('city', tempCity)
+//             .field('state', tempState)
+//             .field('story', tempStory)
+//             .field('userID', tempData.user.user._id)
+//             .field('guardianID', tempData.guardian._id)
+//             .attach('image', `${__dirname}/test-assets/goat.jpg`)
+//             .then((res) => {
+//               expect(res.status).toEqual(200);
+//             });
 //         });
 //     });
 //   });
